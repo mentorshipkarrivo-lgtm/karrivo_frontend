6466a167120d64a16e090a3fb17e78604afe1ca7
mentorshipkarrivo-lgtm <Kgandharv2019@outlook.com>
2026-01-30 00:29:42 +0530

    fixed error

--- src/components/MentorDashboard/MentorSupport/MentorHelpSupport.jsx
+++ src/components/MentorDashboard/MentorSupport/MentorHelpSupport.jsx
@@ -4,8 +4,9 @@ import {
     useGetMentorSupportTicketsQuery, 
     useCreateMentorSupportTicketMutation, 
     useUpdateMentorSupportTicketMutation 
-} from "./MentorSecapislice"
+} from "./MentorSupportapislice"
 
+import 
 const MentorHelpSupport = () => {
     const [mentorId, setMentorId] = useState(null);
     const [isModalOpen, setIsModalOpen] = useState(false);
