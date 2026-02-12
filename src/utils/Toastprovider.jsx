4a24e1379850a40d561eef9f11eec0f7e0ce0ebf
mentorshipkarrivo-lgtm <mentorship.karrivo@gmail.com>
2026-02-13 01:18:29 +0530

    fixed uri issues

--- src/utils/Toastprovider.jsx
+++ src/utils/Toastprovider.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
+
 import './toast-provider.css';
 // Toast utility functions
 export const showToast = {
