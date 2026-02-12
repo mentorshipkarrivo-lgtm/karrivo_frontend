2b5173c8f5ab67bf8b696aa5e7bd56b7e743004a
mentorshipkarrivo-lgtm <mentorship.karrivo@gmail.com>
2026-02-13 01:03:09 +0530

    fixed taosity issue

--- src/utils/Toastprovider.jsx
+++ src/utils/Toastprovider.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import './ToastProvider.css';
-
+import './toast-provider.css';
 // Toast utility functions
 export const showToast = {
   success: (message) => {
